4f2e3eae86aa26eef47c5a343c8a34b79239c457
pchchv <ipchchv@gmail.com>
2022-07-14 23:24:28 +0300

    Implement generate method

--- main.go
+++ main.go
@@ -1,5 +1,10 @@
 package main
 
+func valueGeneration(valType string, valLength int) string {
+	var value string
+	return value
+}
+
 func main() {
 	server()
 }
--- server.go
+++ server.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func ping(w http.ResponseWriter, _ *http.Request) {
@@ -18,8 +19,28 @@ func ping(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+func generate(w http.ResponseWriter, r *http.Request) {
+	valType := r.URL.Query().Get("type")
+	vl := r.URL.Query().Get("length")
+	valLength, err := strconv.Atoi(vl)
+	if err != nil {
+		log.Panic(err)
+	}
+	val := valueGeneration(valType, valLength)
+	v, err := json.Marshal(val)
+	if err != nil {
+		log.Panic(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(v)
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 func server() {
 	log.Println("Server started!")
 	http.HandleFunc("/ping", ping)
+	http.HandleFunc("/generate", generate)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
